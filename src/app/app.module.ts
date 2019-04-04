632c5ec3e5e6927b72877c41d8ecd3df95641b7c
judithachieng <judith.achieng@andela.com>
2019-04-04 22:10:55 +0300

    setup app structure: -add users,login and riders components. -add nav component. -update routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { RidersComponent } from './components/riders/riders.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavComponent,
+    RidersComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
