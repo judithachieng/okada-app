12856c13640ce024270f23eebede68acf4b3bbfe
judithachieng <judith.achieng@andela.com>
2019-04-05 19:23:40 +0300

    add jwt and error interceptors.

---
+++ src/app/_helpers/http-error.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { AuthService } from '../_services/auth/auth.service';
+
+
+/*
+The HttpErrorInterceptor intercepts http responses from the api to check
+if there were any errors. If there is a 401 Unauthorized response the
+user is automatically logged out of the application
+*/
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+  constructor(
+    private authService: AuthService
+  ) { }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(catchError(err => {
+        if (err.status === 401) {
+            // auto logout if 401 response returned from api
+            this.authService.logout();
+            location.reload(true);
+        }
+
+        const error = err.error.message || err.statusText;
+        return throwError(error);
+    }));
+  }
+}
---
+++ src/app/_helpers/jwt.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../_services/auth/auth.service';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+    constructor(private authService: AuthService) { }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        // add authorization header with jwt
+        const currentUser = this.authService.currentUser;
+        if (currentUser && currentUser.token) {
+            request = request.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${currentUser.token}`
+                }
+            });
+        }
+
+        return next.handle(request);
+    }
+}
