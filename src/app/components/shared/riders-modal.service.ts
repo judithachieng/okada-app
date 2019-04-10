50c5d237c97d4e114ec7c50d95b311158b0a28e1
judithachieng <judith.achieng@andela.com>
2019-04-11 00:46:57 +0300

    redesign riders data form.

---
+++ src/app/components/shared/riders-modal.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { FormGroup, FormControl, Validator, Validators} from '@angular/forms';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RidersModalService {
+
+  constructor() { }
+  form: FormGroup = new FormGroup({
+    fullname: new FormControl('', Validators.required),
+    gender: new FormControl('', Validators.required),
+    licenseId: new FormControl('', Validators.required),
+    country: new FormControl('', [Validators.required, Validators.maxLength(2)]),
+    msisdn: new FormControl('', Validators.required),
+    pinNumber: new FormControl('', [Validators.required, Validators.maxLength(4)]),
+  });
+
+  InitializeFormGroup() {
+    this.form.setValue({
+    fullname: 'Jane Doe',
+    gender: 'F',
+    licenseId: 'KE-FG-67J',
+    country: 'KE',
+    msisdn: '+254786000000',
+    pinNumber: '1234'
+    });
+  }
+
+}
